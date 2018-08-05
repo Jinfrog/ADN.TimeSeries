5a45b77cf407039f2eb4e9e1d7c64fc32dcf1c84
Andrés <andresdigiovanni@gmail.com>
2018-08-05 21:26:03 +0200

    Added SmoothedZScore

---
+++ src/ADN.TimeSeries/src/SmoothedZScore.cs
@@ -0,0 +1,70 @@
+﻿using ADN.Helpers.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADN.TimeSeries
+{
+    public class SmoothedZScore
+    {
+        private List<double> _filteredY = new List<double>();
+        private List<double> _avgFilter = new List<double>();
+        private List<double> _stdFilter = new List<double>();
+
+        private double _threshold = 0;
+        private double _influence = 0;
+        private int _lag = 10;
+
+
+        public void SetThreshold(double threshold)
+        {
+            _threshold = threshold;
+        }
+
+        public void SetInfluence(double influence)
+        {
+            _influence = influence;
+        }
+
+        public void SetLag(int lag)
+        {
+            _lag = lag;
+        }
+
+        public int Add(double value)
+        {
+            int signal = 0;
+            int i = _filteredY.Count;
+
+
+            if (i > _lag)
+            {
+                if (Math.Abs(value - _avgFilter[i - 1]) > _threshold * _stdFilter[i - 1])
+                {
+                    signal = (value > _avgFilter[i - 1]) ? 1 : -1;
+
+                    //Make influence lower
+                    _filteredY.Add(_influence * value + (1 - _influence) * _filteredY[i - 1]);
+                }
+                else
+                {
+                    _filteredY.Add(value);
+                }
+
+                //Adjust the filters
+                _avgFilter.Add(ListHelper.Mean(_filteredY, i - _lag, i));
+                _stdFilter.Add(ListHelper.StandardDeviation(_filteredY, i - _lag, i));
+            }
+            else
+            {
+                _filteredY.Add(value);
+
+                //Adjust the filters
+                _avgFilter.Add(ListHelper.Mean(_filteredY, 0, i));
+                _stdFilter.Add(ListHelper.StandardDeviation(_filteredY, 0, i));
+            }
+
+            return signal;
+        }
+    }
+}
---
+++ tests/ADN.TimeSeries.Tests/src/SmoothedZScoreTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ADN.TimeSeries.Tests
+{
+    public class SmoothedZScoreTest
+    {
+        [Theory]
+        [ClassData(typeof(AddData))]
+        public void Add(double[] value, double threshold, double influence, int lag, double[] expected)
+        {
+            var smoothedZScore = new SmoothedZScore();
+            smoothedZScore.SetThreshold(threshold);
+            smoothedZScore.SetInfluence(influence);
+            smoothedZScore.SetLag(lag);
+
+            var result = new List<double>();
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                result.Add(smoothedZScore.Add(value[i]));
+            }
+
+            Assert.Equal(expected, result.ToArray());
+        }
+
+        public class AddData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, -5, -5, -5, -5, -5, -5 },
+                                            2, 5, 5,
+                                            new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0 }};
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
